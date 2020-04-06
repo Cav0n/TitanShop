89be1e3f63c1dd1d46e6a2248acea6117e7af754
Florian Bernard <fbernard@openstudio.fr>
2020-04-06 22:02:51 +0200

    Added Travis

--- config/database.php
+++ config/database.php
@@ -91,6 +91,17 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'testing' => [
+            'driver' => 'mysql',
+            'host' => env('DB_TEST_HOST', 'localhost'),
+            'database' => env('DB_TEST_DATABASE', 'titanshop'),
+            'username' => env('DB_TEST_USERNAME', 'titanshop'),
+            'password' => env('DB_TEST_PASSWORD', ''),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => false,
+          ],
     ],
 
     /*
