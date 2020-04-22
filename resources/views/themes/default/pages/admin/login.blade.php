76091e04d0343a1e32178e6b9f3906b5d4098d5a
Florian Bernard <fbernard@openstudio.fr>
2020-04-22 22:44:51 +0200

    Updated admin login page with shop name

--- resources/views/themes/default/pages/admin/login.blade.php
+++ resources/views/themes/default/pages/admin/login.blade.php
@@ -5,6 +5,8 @@
         <div class="row justify-content-center">
             <div class="col-12 col-md-10 col-lg-8 col-xl-4">
 
+                <h1 class="text-center">{{ \App\Setting::valueOrNull('SHOP_NAME') }}</h1>
+
                 @if(!empty($errors->any()))
                 @include('themes.default.components.alerts.error', ['errors' => $errors->all()])
                 @endif
@@ -12,7 +14,7 @@
                 <form class="row bg-light shadow-sm py-3" action="{{ route('admin.login') }}" method="POST">
                     @csrf
                     <div class="col-12 form-group">
-                        <input type="text" class="form-control {{ $errors->has('login') ? 'is-invalid' : '' }}" name="login" id="login" placeholder="Login" value="{{ old('login') }}">
+                        <input type="text" class="form-control {{ $errors->has('login') ? 'is-invalid' : '' }}" name="login" id="login" placeholder="Email ou pseudo" value="{{ old('login') }}">
                     </div>
                     <div class="col-12 form-group">
                         <input type="password" class="form-control" name="password" id="password" placeholder="Mot de passe">
