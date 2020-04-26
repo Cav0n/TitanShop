fe584c7c88026ee9ae20ab5e80c7f4e516cfca01
Florian Bernard <fbernard@openstudio.fr>
2020-04-26 23:00:11 +0200

    Fixed sidenav width

--- resources/views/themes/default/components/admin/sidenav.blade.php
+++ resources/views/themes/default/components/admin/sidenav.blade.php
@@ -1,4 +1,4 @@
-<div id="sidenav" class="bg-light p-2 shadow-sm h-100">
+<div id="sidenav" class="bg-light p-2 shadow-sm h-100 w-100">
     <h1 class="h4 text-center">Titan Shop</h1>
 
     <div class="mt-3 d-flex flex-column">
