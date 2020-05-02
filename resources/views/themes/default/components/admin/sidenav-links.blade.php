0d6874e048e5e3df9d4405f1357f9eb39b64c747
Florian Bernard <fbernard@openstudio.fr>
2020-05-02 20:02:59 +0200

    Fixed mobile sidenav
    + Added "see website" link in backoffice
    + Added "logout" link in backoffice

--- resources/views/themes/default/components/admin/sidenav-links.blade.php
+++ resources/views/themes/default/components/admin/sidenav-links.blade.php
@@ -1,9 +1,9 @@
 <div class="sidenav mt-3 d-flex flex-column">
-    <div class="row mx-0">
-        <div class="col-2 p-0">
+    <div class="row mx-0 mx-lg-2">
+        <div class="d-none d-lg-flex flex-column col-2 p-0">
             <img src="{{ asset('images/icons/homepage-bw.svg') }}" alt="homepage icon" class="svg w-100 {{ request()->route()->named('admin.index') ? 'active' : null }}" >
         </div>
-        <div class="col-10 d-flex flex-column">
+        <div class="px-0 px-lg-2 col-12 col-lg-10 d-flex flex-column">
             <a href="{{ route('admin.index') }}" class="h5 mb-0 {{ request()->route()->named('admin.index') ? 'active' : null }}">
                 Accueil</a>
             <a href="{{ route('admin.index') }}" class="{{ request()->route()->named('admin.index') ? 'active' : null }}">
@@ -11,11 +11,11 @@
         </div>
     </div>
 
-    <div class="row mx-0 mt-2">
-        <div class="col-2 p-0">
+    <div class="row mx-0 mx-lg-2 mt-2">
+        <div class="d-none d-lg-flex flex-column col-2 p-0">
             <img src="{{ asset('images/icons/orders-bw.svg') }}" alt="orders icon" class="svg w-100 {{ request()->route()->named('admin.orders') ? 'active' : null }}" >
         </div>
-        <div class="col-10 d-flex flex-column">
+        <div class="px-0 px-lg-2 col-12 col-lg-10 d-flex flex-column">
             <a href="{{ route('admin.orders') }}" class="h5 mb-0 {{ request()->route()->named('admin.orders') ? 'active' : null }}">
                 Commandes</a>
             <a href="{{ route('admin.orders') }}" class="{{ request()->route()->named('admin.orders') ? 'active' : null }}">
@@ -23,11 +23,11 @@
         </div>
     </div>
 
-    <div class="row mx-0 mt-2">
-        <div class="col-2 p-0">
+    <div class="row mx-0 mx-lg-2 mt-2">
+        <div class="d-none d-lg-flex flex-column col-2 p-0">
             <img src="{{ asset('images/icons/products-bw.svg') }}" alt="products icon" class="svg w-100 {{ request()->route()->named('admin.products') ? 'active' : null }}">
         </div>
-        <div class="col-10 d-flex flex-column">
+        <div class="px-0 px-lg-2 col-12 col-lg-10 d-flex flex-column">
             <a href="{{ route('admin.products') }}" class="h5 mb-0 {{ request()->route()->named('admin.products') ? 'active' : null }}">
                 Produits</a>
             <a href="{{ route('admin.products') }}" class="{{ request()->route()->named('admin.products') ? 'active' : null }}">
@@ -37,11 +37,11 @@
         </div>
     </div>
 
-    <div class="row mx-0 mt-2">
-        <div class="col-2 p-0">
+    <div class="row mx-0 mx-lg-2 mt-2">
+        <div class="d-none d-lg-flex flex-column col-2 p-0">
             <img src="{{ asset('images/icons/customers-bw.svg') }}" alt="customers icon" class="svg w-100 {{ request()->route()->named('admin.users') ? 'active' : null }}">
         </div>
-        <div class="col-10 d-flex flex-column">
+        <div class="px-0 px-lg-2 col-12 col-lg-10 d-flex flex-column">
             <a href="{{ route('admin.users') }}" class="h5 mb-0 {{ request()->route()->named('admin.users') ? 'active' : null }}">
                 Clients</a>
             <a href="{{ route('admin.users') }}" class="{{ request()->route()->named('admin.users') ? 'active' : null }}">
@@ -49,15 +49,25 @@
         </div>
     </div>
 
-    <div class="row mx-0 mt-2">
-        <div class="col-2 p-0">
-            <img src="{{ asset('images/icons/settings-bw.svg') }}" alt="settings icon" class="svg w-100 h-100 {{ request()->route()->named('admin.settings') ? 'active' : null }}">
+    <div class="row mx-0 mx-lg-2 mt-2">
+        <div class="d-none d-lg-flex flex-column col-2 p-0">
+            <img src="{{ asset('images/icons/settings-bw.svg') }}" alt="settings icon" class="svg w-100 {{ request()->route()->named('admin.settings') ? 'active' : null }}">
         </div>
-        <div class="col-10 d-flex flex-column">
+        <div class="px-0 px-lg-2 col-12 col-lg-10 d-flex flex-column">
             <a href="{{ route('admin.settings') }}" class="h5 mb-0 {{ request()->route()->named('admin.settings') ? 'active' : null }}">
                 Paramètres</a>
             <a href="{{ route('admin.settings') }}" class="{{ request()->route()->named('admin.settings') ? 'active' : null }}">
                 Tous les paramètres</a>
+        </div>
+    </div>
+
+    <div class="row mx-0 mx-lg-2 mt-2">
+        <div class="d-none d-lg-flex flex-column col-2 p-0">
+            <img src="{{ asset('images/icons/logout-bw.svg') }}" alt="settings icon" class="svg w-100">
+        </div>
+        <div class="px-0 px-lg-2 col-12 col-lg-10 d-flex flex-column justify-content-center">
+            <a href="{{ route('admin.logout') }}">
+                Déconnexion</a>
         </div>
     </div>
 </div>
--- resources/views/themes/default/components/admin/sidenav.blade.php
+++ resources/views/themes/default/components/admin/sidenav.blade.php
@@ -1,5 +1,9 @@
 <div id="sidenav" class="bg-white p-2 shadow-sm h-100 w-100">
-    <h1 class="h4 text-center">Titan Shop</h1>
+    <h1 class="h4 text-center mb-0">Titan Shop</h1>
+    <a class="text-muted d-flex justify-content-center" href="{{ route('index') }}">
+        Voir le site
+        <img src="{{ asset('images/icons/eye-bw.svg') }}" class="svg ml-1" alt="eye icon">
+    </a>
 
     @include('themes.default.components.admin.sidenav-links')
 </div>
