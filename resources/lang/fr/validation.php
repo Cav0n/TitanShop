781ea9f1f24f3bc1d7d7d0196018c3975c5073de
Florian Bernard <fbernard@openstudio.fr>
2020-05-03 15:31:32 +0200

    Fixed translation

--- resources/lang/fr/validation.php
+++ resources/lang/fr/validation.php
@@ -2,7 +2,7 @@
 
 return [
 
-    *\
+    /*
     |--------------------------------------------------------------------------
     | FR - Validation des lignes de language
     |--------------------------------------------------------------------------
@@ -11,7 +11,7 @@ return [
     | the validator class. Some of these rules have multiple versions such
     | as the size rules. Feel free to tweak each of these messages here.
     |
-    \*
+    */
 
     'accepted' => 'Le :attribute doit être accepté.',
     'active_url' => 'Le :attribute n\est pas un URL valide.',
@@ -87,7 +87,7 @@ return [
     'min' => [
         'numeric' => 'Le :attribute doit être au moins :min.',
         'file' => 'Le :attribute doit être au moins :min kilobytes.',
-        'string' => 'Le :attribute must be at least :min caractères.',
+        'string' => 'Le :attribute doit contenir au moins :min caractères.',
         'array' => 'Le :attribute doit avoir au moins :min éléments.',
     ],
     'not_in' => 'Le :attribute séléctionné est invalide.',
@@ -118,16 +118,16 @@ return [
     'url' => 'Le :attribute à un format invalide.',
     'uuid' => 'Le :attribute doit être un UUID valide.',
 
-    \*
+    /*
     |--------------------------------------------------------------------------
-    | EN - Custom Validation Language Lines
+    | FR - Custom Validation Language Lines
     |--------------------------------------------------------------------------
     |
     | Here you may specify custom validation messages for attributes using the
     | convention "attribute.rule" to name the lines. This makes it quick to
     | specify a specific custom language line for a given attribute rule.
     |
-    *\
+    */
 
     'custom' => [
         'attribute-name' => [
@@ -135,16 +135,16 @@ return [
         ],
     ],
 
-    \*
+    /*
     |--------------------------------------------------------------------------
-    | EN - Custom Validation Attributes
+    | FR - Custom Validation Attributes
     |--------------------------------------------------------------------------
     |
     | The following language lines are used to swap our attribute placeholder
     | with something more reader friendly such as "E-Mail Address" instead
     | of "email". This simply helps us make our message more expressive.
     |
-    *\
+    */
 
     'attributes' => [],
 
