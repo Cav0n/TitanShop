03ed628a0c393d26d36427f1ca43ba83512037e1
Florian Bernard <cav0n@hotmail.fr>
2020-06-13 16:35:10 +0200

    Added imageI18n creation test

--- tests/Feature/ImageTest.php
+++ tests/Feature/ImageTest.php
@@ -19,15 +19,35 @@ class ImageTest extends TestCase
         $image->save();
 
         $this->assertNotNull($image);
+
+        $imageI18n = self::createI18n();
+
+        $imageI18n->save();
+
+        $this->assertNotNull($imageI18n);
     }
 
     public static function create(
         $path = null
-    )
-    {
+    ) {
         $image = new Image();
         $image->path = $path ?? public_path('images/utils/question-mark.png');
 
         return $image;
     }
+
+    public static function createI18n(
+        $image_id = null,
+        $lang = null,
+        $alt = null,
+        $title = null
+    ) {
+        $imageI18n = new ImageI18n();
+        $imageI18n->image_id = $image_id;
+        $imageI18n->lang = $lang;
+        $imageI18n->alt = $alt;
+        $imageI18n->title = $title;
+
+        return $imageI18n;
+    }  
 }
