c72b8cef988308baafd8fd12aba156bd22a9b649
Florian BERNARD <Cav0n@hotmail.fr>
2020-07-24 11:02:29 +0200

    Updated searchbar in header

--- themes/default/components/header.blade.php
+++ themes/default/components/header.blade.php
@@ -5,9 +5,11 @@
                 <h1>TitanShop</h1>
             </a>
         </div>
-        <div class="header-right d-none d-lg-flex">
-            <div class="d-flex flex-column justify-content-center">
-                <input type="text" placeholder="Search" class="form-control search">
+        <div class="header-right d-none d-lg-flex justify-content-end w-100 pl-3">
+            <div class="d-flex flex-column justify-content-center w-100">
+                <div class="search-container d-flex justify-content-end">
+                    <input type="text" placeholder="Search" class="form-control search header-search transition">
+                </div>
             </div>
             <div class="header-link d-flex flex-column justify-content-center px-3">
                 <a href="#">Mon compte</a>
