10784e50b3813acc49f551da7d5e2c2e31ba025a
Florian BERNARD <Cav0n@hotmail.fr>
2020-08-10 13:43:11 +0200

    Style update in public homepage

--- themes/default/pages/homepage.blade.php
+++ themes/default/pages/homepage.blade.php
@@ -1,7 +1,7 @@
 @extends('default.templates.public')
 
 @section('page.content')
-    <div class="row mb-3">
+    <div class="row mt-2">
         @foreach (App\Models\Category::where('isVisible', 1)->where('isDeleted', 0)->where('parent_id', null)->get() as $category)
         @include('default.components.category-small', ['category' => $category])
         @endforeach
